450b09840477b1cb01a56f52e950c51f0e249fee
belisario-afk <belisario419mz@gmail.com>
2025-08-24 18:06:26 -0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ export default defineConfig({
   base: '/dwdw/',
   resolve: {
     alias: [
+      // Direct file alias to be absolutely sure this resolves
       { find: '@auth/pkce', replacement: r('src/auth/pkce.ts') },
+      // Folder aliases
       { find: '@auth', replacement: r('src/auth') },
       { find: '@spotify', replacement: r('src/spotify') },
       { find: '@audio', replacement: r('src/audio') },
@@ -18,8 +20,5 @@ export default defineConfig({
     ]
   },
   server: { host: '127.0.0.1', port: 5173 },
-  build: {
-    target: 'es2020',
-    sourcemap: true
-  }
+  build: { target: 'es2020' }
 });
